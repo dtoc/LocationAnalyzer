452c5e06083222ebd0aeab32bea5c91ed4094666
dtoci <dtoci@LAPTOP-4LLNOP1K>
2016-08-06 17:51:22 -0500

    Reducing the amount of data we parse out of the page, since grabbing the page gets us a lot of stuff we don't need.

--- LocationAnalyzer/Program.cs
+++ LocationAnalyzer/Program.cs
@@ -17,13 +17,24 @@ namespace LocationAnalyzer
                 States states = new States();
                 Stream stream = client.OpenRead("https://en.wikipedia.org/wiki/Lists_of_populated_places_in_the_United_States");
 
+                File.Delete("C:\\projects\\practice.txt");
+                using (var fc = File.Create("C:\\projects\\practice.txt"))
+                {
+                    fc.Close();
+                }
+
                 StreamReader sr = new StreamReader(stream);
                 while (!sr.EndOfStream)
                 {
                     var currentLine = sr.ReadLine();
-                    if (states.isAState(currentLine))
+                    if (states.isAState(currentLine) && currentLine.Contains("List_of_places_in"))
                     {
                         Console.WriteLine(currentLine);
+                        using (var sw = File.AppendText("C:\\projects\\practice.txt"))
+                        {
+                            sw.WriteLine(currentLine);
+                            sw.Dispose();
+                        }
                     }
                 }
                 Console.ReadKey();
