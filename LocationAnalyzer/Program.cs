8b7b4d3f655b5fe662ed74631272b855c736e46e
dtoci <dtoci@LAPTOP-4LLNOP1K>
2016-08-06 19:04:10 -0500

    Cleaned up logging so that there's only one record per state. Improved performance by a nice margin :D

--- LocationAnalyzer/Program.cs
+++ LocationAnalyzer/Program.cs
@@ -18,6 +18,7 @@ namespace LocationAnalyzer
 
                 // Our starting URL used for seeding the rest of the app with the data and links we'll need later on.
                 Stream stream = client.OpenRead("https://en.wikipedia.org/wiki/Lists_of_populated_places_in_the_United_States");
+                List<State> states = new List<State>();
 
                 // If we already have a file of results, delete it because it's outdated.
                 File.Delete("C:\\projects\\practice.txt");
@@ -33,42 +34,42 @@ namespace LocationAnalyzer
                 {
                     var currentLine = sr.ReadLine();
                     State state = new State();
-                    
+                    var stateName = statesContainer.GetStateName(currentLine);
                     // We only care about webpage lines that 1) reference an American state and 2) contain data on the list of places in that state
-                    if (statesContainer.GetStateName(currentLine) != null && currentLine.Contains("List_of_places_in"))
+                    if (stateName != null && currentLine.Contains("List_of_places_in"))
                     {
                         // Since each line contains multiple bits of useful information, let's tokenize the line.
                         var tokens = currentLine.Split(' ');
-                        foreach (var token in tokens)
-                        {
-                            var stateName = statesContainer.GetStateName(token);
+                        var linkToken = tokens.Where(t => t.Contains("href")).SingleOrDefault();
 
-                            // If the current token contains an href, we want to parse out the link.
-                            if (token.Contains("href"))
-                            {
-                                // We want the substring that no longer has the href in it.
-                                var strippedLink = token.Substring(5);
-                                // We want to remove the first character, since it's a quotation mark.
-                                strippedLink = strippedLink.Remove(0, 1);
-                                // We want to remove the last character, since it's a quotation mark.
-                                strippedLink = strippedLink.Remove(strippedLink.Length - 1, 1);
-                                state.Link = strippedLink;
-                                
-                            }
-                            else if (stateName != null)
-                            {
-                                state.Name = stateName;
-                            }
+                        if (linkToken != null)
+                        {
+                            // We want the substring that no longer has the href in it.
+                            var strippedLink = linkToken.Substring(5);
+                            // We want to remove the first character, since it's a quotation mark.
+                            strippedLink = strippedLink.Remove(0, 1);
+                            // We want to remove the last character, since it's a quotation mark.
+                            strippedLink = strippedLink.Remove(strippedLink.Length - 1, 1);
+                            state.Link = strippedLink;
+                        }
+
+                        state.Name = stateName;
+                        if (!states.Where(s => s.Link == state.Link && s.Name == state.Name).Any())
+                        {
+                            states.Add(state);
                         }
 
                         Console.WriteLine(currentLine);
+                    }
+                }
 
-                        // Logging the results to the log file
-                        using (var sw = File.AppendText("C:\\projects\\practice.txt"))
-                        {
-                            sw.WriteLine(currentLine);
-                            sw.Dispose();
-                        }
+                using (var sw = File.AppendText("C:\\projects\\practice.txt"))
+                {
+                    foreach (var state in states)
+                    {
+                        sw.WriteLine(state.Name);
+                        sw.WriteLine(state.Link);
+                        sw.WriteLine();
                     }
                 }
                 Console.ReadKey();
